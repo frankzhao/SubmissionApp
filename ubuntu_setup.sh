686b3bf413e9815b6480bf3097af65c0b2abe102
Buck Shlegeris <bshlegeris@gmail.com>
2014-01-22 11:43:38 -0800

    goddammit Ubuntu

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,3 +1,12 @@
+# These are instructions for getting the submission app working on a clean
+# Ubuntu install.
+
+# It expects to be run from within the project directory. So start out by cloning
+# my Git repo at https://github.com/BuckShlegeris/SubmissionApp.git
+
+# I've tested this on clean Vagrant installs, and it works there.
+
+# Please feel free to email or Skype me at buckmbs@hotmail.com
 
 # Instructions slightly altered from https://www.digitalocean.com/community/articles/how-to-install-ruby-on-rails-on-ubuntu-12-04-lts-precise-pangolin-with-rvm
 # See there for more details.
@@ -10,19 +19,12 @@ rvm install ruby
 rvm use ruby --default
 rvm rubygems current
 gem install rails --no-ri --no-rdoc
-sudo apt-get install git -y
 
 # This installs a JS runtime, which Rails needs
 sudo apt-get install nodejs -y
 
-# This downloads my code
-git clone https://github.com/BuckShlegeris/SubmissionApp.git
-
-# This sets it up
-cd SubmissionApp
-
+# This installs the Rails dependencies
 bundle install
 sh setup.sh
 
-# This runs it
-rails server
+echo "Run `rails server` to see it running on port 3000"
