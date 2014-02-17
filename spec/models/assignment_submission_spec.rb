0b42ed5e5a19932c9bf2b8dc0eae8ccdbb68079e
Buck Shlegeris <bshlegeris@gmail.com>
2014-02-16 20:57:37 -0800

    force ssl

--- spec/models/assignment_submission_spec.rb
+++ spec/models/assignment_submission_spec.rb
@@ -14,7 +14,9 @@ describe AssignmentSubmission do
                                :group_type => labs,
                                :due_date => "2014-05-03 23:04:26",
                                :behavior_on_submission => '{"you fail": []}')
-        expect(emily.group
+        submission = AssignmentSubmission.create(:user_id => emily.id,
+                                                :assignment_id => wireworld.id)
+        expect(submission.group).to eq(tute)
       end
     end
   end
