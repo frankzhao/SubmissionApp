05c517b20af6bb05ea8eb2ab991a8a3a8399d9a4
Buck Shlegeris <bshlegeris@gmail.com>
2014-02-19 23:36:51 -0800

    minorly prettier

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     @group_type = @group.group_type
     @users = @group.students
+    @staff = @group.staff
     render :show
   end
 end
