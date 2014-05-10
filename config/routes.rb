3a856f8ffb206fa7b1b31519b8751aa412e8bccf
Frank Zhao <frank@frankzhao.net>
2014-05-10 20:15:20 +1000

    fix link to down submission download

--- config/routes.rb
+++ config/routes.rb
@@ -67,9 +67,10 @@ SubmissionApp::Application.routes.draw do
   get 'assignments/:assignment_id/assignment_submissions/:id/file.zip',
                                     :to => 'assignment_submissions#get_zip',
                                     :as => 'assignment_submission_zip'
+  
   get 'assignments/:assignment_id/assignment_submissions/:id/Main.hs',
-                                  :to => 'assignment_submissions#download_raw',
-                                  :as => 'assignment_submission_raw'
+                                    :to => 'assignment_submissions#download_raw',
+                                    :as => 'assignment_submission_raw'
 
   get 'comment_files/:id', :to => 'comments#get_file',
                            :as => 'comment_file'
